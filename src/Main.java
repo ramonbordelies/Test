071c6688858bf6c198988c07db5f6e182482c262
ramonbordelies <bordeliessanchezr1@udayton.edu>
2022-12-13 20:20:18 -0500

    Congratulations! You are free.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Danny is cool!");
+        for(int i = 0; i < 10; i++)
+        {
+            System.out.println("Danny is not cool.");
+        }
+
     }
 }
